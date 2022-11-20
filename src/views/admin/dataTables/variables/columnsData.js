f197e260c6871cd99e52fdc6ba6e468cbd2a9966
FilFip <filip.wnek200@gmail.com>
2022-11-20 02:09:42 +0100

    init commit

--- src/views/admin/dataTables/variables/columnsData.js
+++ src/views/admin/dataTables/variables/columnsData.js
@@ -1,75 +1,75 @@
 export const columnsDataDevelopment = [
   {
-    Header: "NAME",
-    accessor: "name",
+    Header: 'Nazwa',
+    accessor: 'user.name',
   },
   {
-    Header: "TECH",
-    accessor: "tech",
+    Header: 'Zdjęcie',
+    accessor: 'img',
   },
   {
-    Header: "DATE",
-    accessor: "date",
+    Header: 'data',
+    accessor: 'date',
   },
   {
-    Header: "PROGRESS",
-    accessor: "progress",
+    Header: 'Ilość zgłoszeń',
+    accessor: 'reports',
   },
 ];
 
 export const columnsDataCheck = [
   {
-    Header: "NAME",
-    accessor: "name",
+    Header: 'Nazwa',
+    accessor: 'name',
   },
   {
-    Header: "PROGRESS",
-    accessor: "progress",
+    Header: 'PROGRESS',
+    accessor: 'progress',
   },
   {
-    Header: "QUANTITY",
-    accessor: "quantity",
+    Header: 'QUANTITY',
+    accessor: 'quantity',
   },
   {
-    Header: "DATE",
-    accessor: "date",
+    Header: 'data',
+    accessor: 'date',
   },
 ];
 
 export const columnsDataColumns = [
   {
-    Header: "NAME",
-    accessor: "name",
+    Header: 'Nazwa',
+    accessor: 'name',
   },
   {
-    Header: "PROGRESS",
-    accessor: "progress",
+    Header: 'PROGRESS',
+    accessor: 'progress',
   },
   {
-    Header: "QUANTITY",
-    accessor: "quantity",
+    Header: 'QUANTITY',
+    accessor: 'quantity',
   },
   {
-    Header: "DATE",
-    accessor: "date",
+    Header: 'data',
+    accessor: 'date',
   },
 ];
 
 export const columnsDataComplex = [
   {
-    Header: "NAME",
-    accessor: "name",
+    Header: 'Nazwa',
+    accessor: 'name',
   },
   {
-    Header: "STATUS",
-    accessor: "status",
+    Header: 'STATUS',
+    accessor: 'status',
   },
   {
-    Header: "DATE",
-    accessor: "date",
+    Header: 'data',
+    accessor: 'date',
   },
   {
-    Header: "PROGRESS",
-    accessor: "progress",
+    Header: 'PROGRESS',
+    accessor: 'progress',
   },
 ];
--- src/views/admin/default/index.jsx
+++ src/views/admin/default/index.jsx
@@ -21,152 +21,144 @@
 */
 
 // Chakra imports
-import {
-  Avatar,
-  Box,
-  Flex,
-  FormLabel,
-  Icon,
-  Select,
-  SimpleGrid,
-  useColorModeValue,
-} from "@chakra-ui/react";
-// Assets
-import Usa from "assets/img/dashboards/usa.png";
-// Custom components
-import MiniCalendar from "components/calendar/MiniCalendar";
-import MiniStatistics from "components/card/MiniStatistics";
-import IconBox from "components/icons/IconBox";
-import React from "react";
-import {
-  MdAddTask,
-  MdAttachMoney,
-  MdBarChart,
-  MdFileCopy,
-} from "react-icons/md";
-import CheckTable from "views/admin/default/components/CheckTable";
-import ComplexTable from "views/admin/default/components/ComplexTable";
-import DailyTraffic from "views/admin/default/components/DailyTraffic";
-import PieCard from "views/admin/default/components/PieCard";
-import Tasks from "views/admin/default/components/Tasks";
-import TotalSpent from "views/admin/default/components/TotalSpent";
-import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
-import {
-  columnsDataCheck,
-  columnsDataComplex,
-} from "views/admin/default/variables/columnsData";
-import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
-import tableDataComplex from "views/admin/default/variables/tableDataComplex.json";
 
+import { Box, Icon, SimpleGrid, useColorModeValue } from '@chakra-ui/react';
+
+import MiniStatistics from 'components/card/MiniStatistics';
+import IconBox from 'components/icons/IconBox';
+import React from 'react';
+import { MdGroup, MdFileCopy } from 'react-icons/md';
+import { RiPlantFill } from 'react-icons/ri';
+import {
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Divider,
+  ButtonGroup,
+  Button,
+} from '@chakra-ui/react';
+
+import { Card, CardBody, CardFooter } from '@chakra-ui/card';
+import { useEffect, useState } from 'react';
+
+const jwt = process.env.JWT;
 export default function UserReports() {
-  // Chakra Color Mode
-  const brandColor = useColorModeValue("brand.500", "white");
-  const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
-  return (
-    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-      <SimpleGrid
-        columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }}
-        gap='20px'
-        mb='20px'>
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdBarChart} color={brandColor} />
-              }
-            />
-          }
-          name='Earnings'
-          value='$350.4'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdAttachMoney} color={brandColor} />
-              }
-            />
-          }
-          name='Spend this month'
-          value='$642.39'
-        />
-        <MiniStatistics growth='+23%' name='Sales' value='$574.34' />
-        <MiniStatistics
-          endContent={
-            <Flex me='-16px' mt='10px'>
-              <FormLabel htmlFor='balance'>
-                <Avatar src={Usa} />
-              </FormLabel>
-              <Select
-                id='balance'
-                variant='mini'
-                mt='5px'
-                me='0px'
-                defaultValue='usd'>
-                <option value='usd'>USD</option>
-                <option value='eur'>EUR</option>
-                <option value='gba'>GBA</option>
-              </Select>
-            </Flex>
-          }
-          name='Your balance'
-          value='$1,000'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
-              icon={<Icon w='28px' h='28px' as={MdAddTask} color='white' />}
-            />
-          }
-          name='New Tasks'
-          value='154'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdFileCopy} color={brandColor} />
-              }
-            />
-          }
-          name='Total Projects'
-          value='2935'
-        />
-      </SimpleGrid>
+  const [data, setData] = useState([]);
 
-      <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px'>
-        <TotalSpent />
-        <WeeklyRevenue />
-      </SimpleGrid>
-      <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px'>
-        <CheckTable columnsData={columnsDataCheck} tableData={tableDataCheck} />
-        <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
-          <DailyTraffic />
-          <PieCard />
-        </SimpleGrid>
-      </SimpleGrid>
-      <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px'>
-        <ComplexTable
-          columnsData={columnsDataComplex}
-          tableData={tableDataComplex}
+  const brandColor = useColorModeValue('brand.500', 'white');
+  const boxBg = useColorModeValue('secondaryGray.300', 'whiteAlpha.100');
+
+  const getReports = async () => {
+    const req = await fetch('http://10.250.161.137:5011/admin/getReportPosts', {
+      headers: {
+        Authorization: `Bearer ${'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjliMGY1NzFhLTAwNjMtNDNmMy1hNDJjLWJkZDEwNjgyMWViZiIsImVtYWlsIjoiZ292QGdvdi5wbCIsIm5hbWUiOiJnb3YiLCJoYXZlQXZhdGFyIjoiRmFsc2UiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE2Njg4OTc1NzYsImV4cCI6MTcyOTM3NzU3NiwiaWF0IjoxNjY4ODk3NTc2LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdCJ9.GKGYC9Q5tEl3X_YQBRotSn1vA_OFRhrkeIgLU7JXBgqi4lvYMO795i56Dt2cejv-4MxK1MQ9GouPvQywYIQkWA'}`,
+      },
+    });
+    const res = await req.json();
+
+    setData(res);
+  };
+
+  useEffect(() => getReports(), []);
+
+  const banPost = async (id) => {
+    await fetch('http://10.250.161.137:5011/admin/banPost', {
+      headers: {
+        Authorization: `Bearer ${'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjliMGY1NzFhLTAwNjMtNDNmMy1hNDJjLWJkZDEwNjgyMWViZiIsImVtYWlsIjoiZ292QGdvdi5wbCIsIm5hbWUiOiJnb3YiLCJoYXZlQXZhdGFyIjoiRmFsc2UiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE2Njg4OTc1NzYsImV4cCI6MTcyOTM3NzU3NiwiaWF0IjoxNjY4ODk3NTc2LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdCJ9.GKGYC9Q5tEl3X_YQBRotSn1vA_OFRhrkeIgLU7JXBgqi4lvYMO795i56Dt2cejv-4MxK1MQ9GouPvQywYIQkWA'}`,
+      },
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ postId: id }),
+    });
+    getReports();
+  };
+
+  const acceptPost = async (id) => {
+    await fetch('http://10.250.161.137:5011/admin/acceptPost', {
+      headers: {
+        Authorization: `Bearer ${'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjliMGY1NzFhLTAwNjMtNDNmMy1hNDJjLWJkZDEwNjgyMWViZiIsImVtYWlsIjoiZ292QGdvdi5wbCIsIm5hbWUiOiJnb3YiLCJoYXZlQXZhdGFyIjoiRmFsc2UiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE2Njg4OTc1NzYsImV4cCI6MTcyOTM3NzU3NiwiaWF0IjoxNjY4ODk3NTc2LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdCJ9.GKGYC9Q5tEl3X_YQBRotSn1vA_OFRhrkeIgLU7JXBgqi4lvYMO795i56Dt2cejv-4MxK1MQ9GouPvQywYIQkWA'}`,
+      },
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ postId: id }),
+    });
+    getReports();
+  };
+
+  return (
+    <Box pt={{ base: '130px', md: '80px', xl: '80px' }}>
+      <SimpleGrid
+        columns={{ base: 1, md: 2, lg: 3, '2xl': 6 }}
+        gap='20px'
+        mb='100px'>
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w='56px'
+              h='56px'
+              bg={boxBg}
+              icon={
+                <Icon w='32px' h='32px' as={RiPlantFill} color={brandColor} />
+              }
+            />
+          }
+          name='Ilosc Gówna'
+          value='350.4'
         />
-        <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
-          <Tasks />
-          <MiniCalendar h='100%' minW='100%' selectRange={false} />
-        </SimpleGrid>
+        <MiniStatistics
+          startContent={
+            <IconBox
+              w='56px'
+              h='56px'
+              bg={boxBg}
+              icon={<Icon w='32px' h='32px' as={MdGroup} color={brandColor} />}
+            />
+          }
+          name='Ilość użytkowników'
+          value='649'
+        />
       </SimpleGrid>
+      {data.map((item) => (
+        <Card maxW='sm'>
+          <CardBody>
+            <Image
+              src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+              alt='Green double couch with wooden legs'
+              borderRadius='lg'
+            />
+            <Stack mt='6' spacing='3'>
+              <Heading size='md'>{item.user.name}</Heading>
+
+              <Text color='blue.600' fontSize='2xl'>
+                {item.post.reportCount} zgłoszeń ({item.post.amount} kg
+                kompostu)
+              </Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+          <CardFooter mt='20px'>
+            <ButtonGroup spacing='2'>
+              <Button
+                variant='solid'
+                colorScheme='red'
+                onClick={() => banPost(item.post.id)}>
+                Usuń post
+              </Button>
+              <Button
+                variant='ghost'
+                colorScheme='green'
+                onClick={() => acceptPost(item.post.id)}>
+                Zatwierdź post
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
+      ))}
     </Box>
   );
 }
